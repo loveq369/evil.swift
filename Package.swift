3f5941f48695c21f9d2617612e910593bd9ee4dd
Sindre Sorhus <sindresorhus@gmail.com>
2018-01-02 01:56:24 +0100

    Upgrade to Swift 5

--- Package.swift
+++ Package.swift
@@ -1,8 +1,11 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
 	name: "Evil",
+	platforms: [
+		.macOS(.v10_12)
+	],
 	targets: [
 		.target(
 			name: "Evil"
